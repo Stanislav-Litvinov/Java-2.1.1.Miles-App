31bea15b57faa160812ff4b44c00312a96a5559c
STANISLAV LITVINOV <91020862+Stanislav-Litvinov@users.noreply.github.com>
2021-11-29 16:38:52 +0300

    Added Miles.java

---
+++ src/MilesApp.java
@@ -0,0 +1,7 @@
+public class MilesApp {
+    public static void main(String[] args) {
+        int ticketPrice = 15000;
+        int miles = ticketPrice / 20;
+            System.out.println("Вам начислят " + miles + " миль.");
+        }
+    }
